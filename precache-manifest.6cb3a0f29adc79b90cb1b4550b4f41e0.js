55ca1b574bde26783be141abaad42413f3e9a2d2
Nooraldeen Ali <nooraldeen@Nooraldeens-MacBook-Pro.local>
2020-08-18 18:22:06 +0200

    Updates

---
+++ precache-manifest.6cb3a0f29adc79b90cb1b4550b4f41e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81235900e37672e2be457d65512f41d2",
+    "url": "/noor-movies-app/index.html"
+  },
+  {
+    "revision": "217ca15b08e11ddf6507",
+    "url": "/noor-movies-app/static/css/main.062964e0.chunk.css"
+  },
+  {
+    "revision": "1a282663cbe878074f17",
+    "url": "/noor-movies-app/static/js/2.9e422d97.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/noor-movies-app/static/js/2.9e422d97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "217ca15b08e11ddf6507",
+    "url": "/noor-movies-app/static/js/main.b1296556.chunk.js"
+  },
+  {
+    "revision": "0443c9c8bc49bd2176b5",
+    "url": "/noor-movies-app/static/js/runtime-main.ea7d3d22.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d67d445570d788a3300c420bea293cb9.js"
+  "/noor-movies-app/precache-manifest.6cb3a0f29adc79b90cb1b4550b4f41e0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/noor-movies-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
