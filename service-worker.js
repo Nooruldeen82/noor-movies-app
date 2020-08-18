d2f4cd61b3e6e5e60c90ff71ec27b39b749451fe
Nooraldeen Ali <nooraldeen@Nooraldeens-MacBook-Pro.local>
2020-08-18 11:50:10 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/noor-movies-app/precache-manifest.722b8ebf52e40369ccbe24248a913e50.js"
+  "/noor-movies-app/precache-manifest.f1fe75d27337104ef26be612d8d47e7f.js"
 );
 
 self.addEventListener('message', (event) => {
