4bbb3dc84950d1d5be60e6dda2c050fce8c7adb4
Nooraldeen Ali <nooraldeen@Nooraldeens-MacBook-Pro.local>
2020-08-18 11:42:13 +0200

    Updates

---
+++ precache-manifest.722b8ebf52e40369ccbe24248a913e50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eebea897282d7e0455b09a584cc8ba97",
+    "url": "/noor-movies-app/index.html"
+  },
+  {
+    "revision": "eef40b89b8de2eb423bf",
+    "url": "/noor-movies-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e10131e1587f687f514b",
+    "url": "/noor-movies-app/static/js/2.b3dc571a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/noor-movies-app/static/js/2.b3dc571a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eef40b89b8de2eb423bf",
+    "url": "/noor-movies-app/static/js/main.7935be8f.chunk.js"
+  },
+  {
+    "revision": "0443c9c8bc49bd2176b5",
+    "url": "/noor-movies-app/static/js/runtime-main.ea7d3d22.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/noor-movies-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
